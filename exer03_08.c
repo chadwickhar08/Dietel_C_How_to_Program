9b84f7e03fc16560415e0f3bf54dd3db24d51f82
Chad Harris <chadwickhar08@gmail.com>
2018-01-11 16:06:56 -0500

    Adding Exercise 3.8

---
+++ exer03_08.c
@@ -0,0 +1,34 @@
+/*Exercise 3.8 in Chapter 3 */
+
+/*Write a C program that uses the statements in Exercise 3.7 to calculate x raised to the y power. Program should have a while repetition control statement*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    /*Raise x to the power of some number, initialize variables*/
+    int counter = 1;
+    int x, power;
+    int result = 1;
+    
+    /*ask for input from the user for the number and power*/
+    
+    printf("Please enter the number: \n");
+    scanf("%d", &x);
+    
+    printf("Please enter the power to raise the number to: \n");
+    scanf("%d", &power);
+    
+    /*use while construct to calculate power*/
+    
+    while(counter <= power)
+    {
+        result *= x;
+        counter++;
+    }
+    
+    printf("The result is %d\n", result);
+    
+    
+    return 0;
+}
